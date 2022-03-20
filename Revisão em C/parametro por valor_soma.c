17d15e652a1f06c26a5ee2ef0615b080c05d106b
Mariane Andrade <86253405+SpeciousSoup956@users.noreply.github.com>
2022-03-20 13:03:17 -0300

    Add file

---
+++ Revisão em C/parametro por valor_soma.c
@@ -0,0 +1,32 @@
+/**************************************
+REVISÃO DE PROGRAMAÇÃO - LINGUAGEM C
+disciplina: ESTRUTURA DE DADOS (ESTC3)
+assunto: parâmetros por valor 
+***************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma (int n1, int n2) //soma recebe n1, n2 e retorna um inteiro
+{
+  int total; //atribui variavel
+  total = n1+n2; //faz a operação de somar
+  return (total); // retorna o valor da operação para main
+}
+
+int main (void)
+{
+  //variavel
+  int v1, v2, resultado;
+    
+  printf("Digite o primeiro valor: ");
+  scanf("%d", &v1);
+  printf("Digite o segundo valor: ");
+  scanf("%d", &v2);
+    
+  //chama a função e recebe o retorno
+  resultado = soma(v1,v2);
+  
+  printf("Resultado = %d\n", resultado);
+  
+  return 0;
+}
